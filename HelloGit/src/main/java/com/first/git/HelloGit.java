120a6cd96e61bb12e1cbb4d28ef78becf926839f
Ivan <852484201@qq.com>
2021-03-04 22:56:08 +0800

    添加测试文件

---
+++ HelloGit/src/main/java/com/first/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.first.git;
+
+/**
+ * @author Ivan
+ * @create 2021-03-04-22:45
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
